45b52f61c811d5a214d808349a62e6835e68af51
memetibryan <memetibryan@gmail.com>
2017-07-06 16:27:42 +0300

    Successful

--- lib/gamer_one.rb
+++ lib/gamer_one.rb
@@ -3,6 +3,7 @@ class String
 	@@cards = %w{ 2 3 4 5 6 7 8 9 10 J Q K A D S C} #writes the arrays in strings
     @@shuffled_cards = @@cards.shuffle
     @@player_two = @@shuffled_cards.last(8)
+    @@stolen_cards = []
     @@points_one = []
 
 	def a
@@ -10,6 +11,7 @@ class String
 
 		if @@player_two.include?(char_1)
 			@@points_one.push (10)
+			@@stolen_cards.push (char_1)
 
 			total = @@points_one.sum
 			return total
